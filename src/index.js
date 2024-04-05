74b345e02f140d4090f69aea13510d23f6db6471
pbsousa <pbs@isep.ipp.pt>
2024-04-05 17:18:55 +0100

    Delete src/index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-
-import {convertTimetoSeconds} from './lab01/ex01'
-
-let message: string = 'ISEP-DESOFT 1  Lab exercises';
-console.log(message);
-
-
-let seconds = convertTimetoSeconds(10, 20, 30);
-console.log("Seconds: " + seconds);
