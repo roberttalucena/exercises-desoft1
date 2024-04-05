7e040ca813d0290cbc1b20a1b56f50fd51b3ef84
pbsousa <pbs@isep.ipp.pt>
2024-04-05 15:36:32 +0100

    Update ex01.ts

--- src/lab01/ex01.ts
+++ src/lab01/ex01.ts
@@ -1,1 +1,13 @@
+/*
+Write the function convertTimeToSeconds(hours: number, minutes:number, seconds:number):number that given time in hours, minutes and seconds convert it to seconds.
 
+*/
+
+function convertTimetoSeconds(hours: number, minutes: number, seconds: number): number {
+
+    let sec: number = hours * 3600 + minutes * 60 + seconds;
+    return sec;
+}
+
+
+export {convertTimetoSeconds};
