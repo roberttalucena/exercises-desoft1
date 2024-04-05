7ccfc58a1d556958609d77ba8d9a892d6f217de7
pbsousa <pbs@isep.ipp.pt>
2024-04-05 15:38:56 +0100

    Update ex01.test.ts

--- src/__tests__/lab01/ex01.test.ts
+++ src/__tests__/lab01/ex01.test.ts
@@ -1,1 +1,16 @@
+import {convertTimetoSeconds} from '../../lab01/ex01'
 
+
+describe('Testing the convert time to seconds function', () => {
+    test(' 10 hours, 20 minutes, 30 seconds: pass', () => {
+        //Arrange
+        let hours: number = 10;
+        let minutes: number = 20;
+        let seconds: number = 30;
+        let expected: number = 37230;
+        //Act
+        let result = convertTimetoSeconds(hours, minutes, seconds);
+        //Assert
+        expect(result).toBe(expected);
+    });
+});
